fb1cc923897e09524784283f27dbbde17930bd42
TheFiffty <paul.marcelet@ik.me>
2023-04-22 22:59:16 +0200

    creation of the first page with no scrollbar

--- 1.0.1/index.js
+++ 1.0.1/index.js
@@ -1,20 +1,36 @@
-//fonction qui donne la résolution de l'écran
+//fonction qui donne la résolution (taille) de l'écran
 function getResolution() 
 {
   const realWidth = window.screen.width * window.devicePixelRatio;
   const realHeight = window.screen.height * window.devicePixelRatio;
+
+  document.documentElement.style.setProperty('--wScreen', `${realWidth}px`);
+  document.documentElement.style.setProperty('--hScreen', `${realHeight}px`);
+  // document.documentElement.style.setProperty('--wScreen', window.screen.width  + 'px');
+  // document.documentElement.style.setProperty('--hScreen', window.screen.height  + 'px');
+
   console.log(`Your screen resolution is: ${realWidth} x ${realHeight}`);
+  console.log("The screen's size is :" + window.screen.width * window.devicePixelRatio + " x " + window.screen.height * window.devicePixelRatio + " pixels");
+  
 };
 
 //récupération de la taille de la fenêtre
 function getWindowSize() 
 {
+  // Récupération de la largeur de la fenêtre du navigateur
+  // var windowWidth = window.innerWidth;
+  // Récupération de la largeur de la fenêtre visible (sans la bande de déroulement)
+  // var visibleWidth = document.documentElement.clientWidth;
+  // Calcul de la largeur de la bande de déroulement
+  // var scrollbarWidth = windowWidth - visibleWidth;
+
 	console.log("The window's size is :" + window.innerWidth + " x " + window.innerHeight + " pixels");
-  document.documentElement.style.setProperty('--wF', window.innerWidth + 'px');
-  document.documentElement.style.setProperty('--hF', window.innerHeight + 'px');
+  console.log("The window's size 2 is :" + document.documentElement.clientWidth + " x " + document.documentElement.clientHeight + " pixels");
+
+  document.documentElement.style.setProperty('--wWindow', document.documentElement.clientWidth + 'px');
+  document.documentElement.style.setProperty('--hWindow', document.documentElement.clientHeight + 'px');
 
 };
-
 
 //utlilisation des fonctions au chargement
 window.onload = function()
